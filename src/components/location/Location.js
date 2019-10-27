7d85b0df35d268021a26fe2afe8f5dc0ef838a63
Christopher Najafi <chris.najafi@gmail.com>
2019-10-27 16:24:56 -0400

    turned search bar component into class and added foursquare api

--- src/components/location/Location.js
+++ src/components/location/Location.js
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
-import axios from "axios";
 
 class Location extends Component {
 	state = {
 		latitude: "",
-		longitude: "",
-		searchQuery: "",
-		results: [],
-		singleResult: {}
+		longitude: ""
+		// searchQuery: "",
+		// results: [],
+		// singleResult: {}
 	};
-
-	componentDidMount() {
-		this.getLocation();
-		// console.log("WEBPACK IS WORKING");
-	}
 
 	getLocation = () => {
 		navigator.geolocation.getCurrentPosition((response) => {
@@ -24,25 +18,9 @@ class Location extends Component {
 		});
 	};
 
-	getVenues = async () => {
-		try {
-			let query = this.state.searchQuery;
-			let ll = `${this.state.latitude},${this.state.longitude}`;
-			let client_id = "POJRSULYK3BL2FVPTOFBLX40F0B3PMCPCPEF2SHMSQWYEGFT";
-			let client_secret = "PNLMONJFCXLUUVSF5SB22SAFI5VK3UZZB4OVW4DW1D13OKTN";
-			let v = "20140303";
-
-			const queryTemplate = `https://api.foursquare.com/v2/venues/search?query=${query}&ll=${ll}&client_id=${client_id}&client_secret=${client_secret}&v=${v}`;
-			const response = await axios.get(queryTemplate);
-
-			const results = response.data.response.venues;
-			console.log("venues: ", results);
-			this.setState({ results });
-			console.log("first venue", results[0]);
-		} catch (err) {
-			console.error(err);
-		}
-	};
+	componentDidMount() {
+		this.getLocation();
+	}
 
 	render() {
 		return <div></div>;
--- src/components/search-bar/SearchBar.js
+++ src/components/search-bar/SearchBar.js
@@ -1,21 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const SearchBar = () => {
-	return (
-		<div>
-			<form className="form-inline">
-				<input
-					className="form-control mr-sm-2"
-					type="search"
-					placeholder="Search"
-					aria-label="Search"
-				></input>
-				<button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-					Search
-				</button>
-			</form>
-		</div>
-	);
-};
+class SearchBar extends Component {
+	state = {
+		latitude: "",
+		longitude: ""
+		// searchQuery: "",
+		// results: [],
+		// singleResult: {}
+	};
+
+	getVenues = async () => {
+		try {
+			let query = this.state.searchQuery;
+			let ll = `${this.state.latitude},${this.state.longitude}`;
+			let client_id = "POJRSULYK3BL2FVPTOFBLX40F0B3PMCPCPEF2SHMSQWYEGFT";
+			let client_secret = "PNLMONJFCXLUUVSF5SB22SAFI5VK3UZZB4OVW4DW1D13OKTN";
+			let v = "20140303";
+
+			const queryTemplate = `https://api.foursquare.com/v2/venues/search?query=${query}&ll=${ll}&client_id=${client_id}&client_secret=${client_secret}&v=${v}`;
+			const response = await axios.get(queryTemplate);
+
+			const results = response.data.response.venues;
+			console.log("venues: ", results);
+			this.setState({ results });
+			console.log("first venue", results[0]);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	render() {
+		return (
+			<div>
+				<form className="form-inline">
+					<input
+						className="form-control mr-sm-2"
+						type="search"
+						placeholder="Search"
+						aria-label="Search"
+					></input>
+					<button
+						className="btn btn-outline-success my-2 my-sm-0"
+						type="submit"
+					>
+						Search
+					</button>
+				</form>
+			</div>
+		);
+	}
+}
 
 export default SearchBar;
