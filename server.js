326a59aee5aeceb9e055b41234d91b3e3f00f8ed
Christopher Najafi <chris.najafi@gmail.com>
2019-10-28 11:37:18 -0400

    added server

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+// const connectDB = require("./config/db");
+const path = require("path");
+const app = express();
+
+// Connect database
+// connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// app.get("/", (req, res) => res.send(`<h1>Server started on port ${PORT}</h1>`));
+
+// Define routes
+// app.use("/api/users", require("./routes/users"));
+// app.use("/api/auth", require("./routes/auth"));
+// app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	);
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log("############################################");
+	console.log(`-------- Server started on port ${PORT} -------`);
+	console.log(`---------- http://localhost:${PORT} -----------`);
+	console.log("############################################");
+});
