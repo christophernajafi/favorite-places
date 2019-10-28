5a4838f7c95bc5173d1754769d1b17942313053c
Christopher Najafi <chris.najafi@gmail.com>
2019-10-28 12:55:53 -0400

    added user routes, yelp route works for hard-coded query, added auth middleware, added dotenv for environmental variables

--- routes/yelp.js
+++ routes/yelp.js
@@ -1,4 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+const axios = require("axios");
+require("dotenv").config();
 
-// router.get("/");
+router.get(
+	"/",
+	// auth,
+
+	async (req, res, err) => {
+		try {
+			let endPoint = "https://api.yelp.com/v3/businesses/search";
+			let tokenStr = process.env.YELP_API_KEY;
+
+			let queryStr =
+				"https://api.yelp.com/v3/businesses/search?location=boston&term=burrito+sushi+noodles";
+
+			axios.defaults.headers.common["Authorization"] = `Bearer ${tokenStr}`;
+
+			// console.log("ll: ", ll);
+
+			const response = await axios.get(queryStr, {
+				headers: { Authorization: `Bearer ${tokenStr}` }
+			});
+			// console.log("before response");
+
+			console.log("response", response.data);
+
+			// const results = response.data.response.venues;
+			// console.log("results: ", results);
+		} catch {
+			// console.error(err.message);
+			res.status(500).send("Server Error");
+		}
+	}
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json({ extended: false }));
 // app.get("/", (req, res) => res.send(`<h1>Server started on port ${PORT}</h1>`));
 
 // Define routes
+app.use("/api/yelp", require("./routes/yelp"));
 // app.use("/api/users", require("./routes/users"));
 // app.use("/api/auth", require("./routes/auth"));
 // app.use("/api/contacts", require("./routes/contacts"));
