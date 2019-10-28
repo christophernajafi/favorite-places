3abbe60ef116d9c443976f517546f78c51418cf7
Christopher Najafi <chris.najafi@gmail.com>
2019-10-28 13:04:59 -0400

    cleanup up code in yelp get route

--- routes/yelp.js
+++ routes/yelp.js
@@ -11,25 +11,16 @@ router.get(
 
 	async (req, res, err) => {
 		try {
-			let endPoint = "https://api.yelp.com/v3/businesses/search";
-			let tokenStr = process.env.YELP_API_KEY;
+			const endPoint = "https://api.yelp.com/v3/businesses/search";
+			const yelpKey = process.env.YELP_API_KEY;
 
 			let queryStr =
-				"https://api.yelp.com/v3/businesses/search?location=boston&term=burrito+sushi+noodles";
+				"https://api.yelp.com/v3/businesses/search?location=cranford&term=pizza";
 
-			axios.defaults.headers.common["Authorization"] = `Bearer ${tokenStr}`;
+			axios.defaults.headers.common["Authorization"] = `Bearer ${yelpKey}`;
 
-			// console.log("ll: ", ll);
-
-			const response = await axios.get(queryStr, {
-				headers: { Authorization: `Bearer ${tokenStr}` }
-			});
-			// console.log("before response");
-
+			const response = await axios.get(queryStr);
 			console.log("response", response.data);
-
-			// const results = response.data.response.venues;
-			// console.log("results: ", results);
 		} catch {
 			// console.error(err.message);
 			res.status(500).send("Server Error");
