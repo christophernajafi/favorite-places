01f9bdc09b3487a6d5a0fb9d9af1857581f22d93
Christopher Najafi <chris.najafi@gmail.com>
2019-10-31 19:13:56 -0400

    started middleware

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,10 +1,39 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import searchResultsReducer from "./searchResultsReducer";
 
 const reducer = combineReducers({
 	search: searchResultsReducer
 });
 
+// const middleware
+
+// const devMiddlewares=[]
+
+// const prodMiddlewares=[]
+
+// if (process.env.NODE_ENV !== 'production') {
+// 	middleware = composeWithDevTools(
+// 			applyMiddleware(
+// 					thunk,
+// 					cartMiddleware,
+// 					session,
+// 					logger(),
+// 					queryMiddleware(getQueries, getEntities)
+// 			)
+// 	)
+// } else {
+// 	middleware = applyMiddleware(
+// 			thunk,
+// 			cartMiddleware,
+// 			session,
+// 			queryMiddleware(getQueries, getEntities)
+// 	)
+// }
+
 const store = createStore(reducer);
+// const store = createStore(reducer, middleware);
 
 export default store;
