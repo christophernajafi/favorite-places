947052b710d3b45adeb4633174bd9284b2f48756
Christopher Najafi <chris.najafi@gmail.com>
2019-10-30 20:06:20 -0400

    added thunk for getting search results and action creator for setting results on state

--- client/src/redux/searchResultsReducer.js
+++ client/src/redux/searchResultsReducer.js
@@ -4,25 +4,46 @@ import axios from "axios";
  * ACTION TYPES
  */
 
-const SET_RESULTS = "SET_RESULTS";
-const SET_LOCATION = "SET_LOCATION";
-const SET_TERMS = "SET_TERMS";
+const SET_SEARCH_RESULTS = "SET_SEARCH_RESULTS";
+const SET_SEARCH_LOCATION = "SET_SEARCH_LOCATION";
+const SET_SEARCH_TERMS = "SET_SEARCH_TERMS";
+const SET_LAT_LONG = "SET_LAT_LONG";
 
 /**
  * ACTION CREATORS
  */
 
-const setSearchResults = () => {
-	type: SET_RESULTS;
+const setSearchResults = (results) => {
+	type: SET_SEARCH_RESULTS;
+	results;
+};
+
+const setSearchLocation = (location) => {
+	type: SET_SEARCH_LOCATION;
+};
+
+const setSearchTerms = (terms) => {
+	type: SET_SEARCH_TERMS;
+};
+
+const setLatLong = () => {
+	type: SET_LAT_LONG;
 };
 
 /**
  * ACTION THUNKS
  */
 
-export const getSearchResults = () => async (dispatch) => {
+export const getSearchResults = (terms, location) => async (dispatch) => {
 	try {
-		const { data } = await axios.get("/api/yelp");
+		const { data } = await axios.get("/api/yelp", {
+			params: {
+				terms,
+				location
+			}
+		});
+		console.log("data from redux: ", data);
+		dispatch(setSearchResults(data));
 	} catch (err) {
 		console.log(err);
 	}
@@ -47,8 +68,8 @@ const initialState = {
 const searchResultsReducer = (state = initialState, action) => {
 	const newState = { ...state };
 	switch (action.type) {
-		case SET_RESULTS:
-			newState.searchResults = [...newState.searchResults];
+		case SET_SEARCH_RESULTS:
+			newState.searchResults = [...action.searchResults];
 			return newState;
 		default:
 			return state;
