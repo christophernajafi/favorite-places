e71a791fdc2cb1a3a805e01004b6c63d69b23368
Christopher Najafi <chris.najafi@gmail.com>
2019-11-05 13:34:56 -0500

    updated text in about component

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -5,8 +5,12 @@ const About = () => {
 	return (
 		<Fragment>
 			<div className="jumbotron">
-				Hello, welcome to my portfolio project. If you would like to see my
-				other projects, please visit. ChrisNajafi.com. Thanks for visiting!
+				Hello! Welcome to my portfolio project. If you would like to see my
+				other projects, please visit{" "}
+				<a href="https://wwww.chrisnajafi.com/" target="_blank">
+					ChrisNajafi.com
+				</a>
+				.
 			</div>
 		</Fragment>
 	);
