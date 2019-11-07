26d1b0ca84798f7635a41c913f50715e6d04d715
Christopher Najafi <chris.najafi@gmail.com>
2019-11-06 22:33:18 -0500

    added bootstrap classes to login and signup components

--- client/src/components/log-in/LogIn.js
+++ client/src/components/log-in/LogIn.js
@@ -1,11 +1,48 @@
 import React, { Fragment } from "react";
 import "./log-in.css";
 
-const LogIn = () => {
+const LogIn = (props) => {
+	const onChange = (event) => {
+		//
+	};
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+	};
+
 	return (
-		<Fragment>
-			<div className="jumbotron">Log In</div>
-		</Fragment>
+		<div className="form-container">
+			<h1>
+				Account <span className="text-primary">Login</span>
+			</h1>
+			<form onSubmit={onSubmit}>
+				<div className="form-group">
+					<label htmlFor="email">Email Address</label>
+					<input
+						type="email"
+						name="email"
+						value={email}
+						onChange={onChange}
+						required
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						name="password"
+						value={password}
+						onChange={onChange}
+						required
+					/>
+				</div>
+				<input
+					type="submit"
+					value="Login"
+					className="btn btn-primary btn-block"
+				/>
+			</form>
+		</div>
 	);
 };
 
--- client/src/components/sign-up/SignUp.js
+++ client/src/components/sign-up/SignUp.js
@@ -1,11 +1,64 @@
 import React, { Fragment } from "react";
 import "./sign-up.css";
 
-const SignUp = () => {
+const SignUp = (props) => {
+	const onChange = (event) => {
+		//
+	};
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+	};
+
 	return (
-		<Fragment>
-			<div className="jumbotron">Sign Up</div>
-		</Fragment>
+		<div className="form-container">
+			<h1>
+				Account <span className="text-primary">Register</span>
+			</h1>
+			<form onSubmit={onSubmit}>
+				<div className="form-group">
+					<label htmlFor="name">Name</label>
+					<input type="text" name="name" value={name} onChange={onChange} />
+				</div>
+				<div className="form-group">
+					<label htmlFor="email">Email Address</label>
+					<input
+						type="email"
+						name="email"
+						value={email}
+						onChange={onChange}
+						required
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						name="password"
+						value={password}
+						onChange={onChange}
+						required
+						minLength="6"
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="password">Confirm Password</label>
+					<input
+						type="password"
+						name="password2"
+						value={password2}
+						onChange={onChange}
+						required
+						minLength="6"
+					/>
+				</div>
+				<input
+					type="submit"
+					value="Register"
+					className="btn btn-primary btn-block"
+				/>
+			</form>
+		</div>
 	);
 };
 
