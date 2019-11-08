78bc53ed7b2bc6f5633e1458a0d1aed2b3c027ba
Christopher Najafi <chris.najafi@gmail.com>
2019-11-07 20:37:14 -0500

    worked on styling search results

--- client/src/components/search-results/single-result/SingleSearchResult.js
+++ client/src/components/search-results/single-result/SingleSearchResult.js
@@ -2,16 +2,27 @@ import React from "react";
 import "./single-result.css";
 
 const SingleSearchResult = (props) => {
-	const { name, image_url, location, display_phone } = props.result;
+	const {
+		name,
+		image_url,
+		location,
+		display_phone,
+		rating,
+		review_count,
+		categories
+	} = props.result;
 	const { display_address } = location;
-
-	// console.log(typeof is_closed);
+	const { title } = categories[0];
 
 	return (
 		<div>
 			<img src={image_url} alt="" />
 			<br />
 			<span className="font-weight-bold">{name}</span>
+			<br />
+			{rating} ({review_count})
+			<br />
+			{title}
 			<br />
 			{display_address[0] + " " + display_address[1]}
 			<br />
