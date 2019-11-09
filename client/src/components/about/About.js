b36f2fd6cce2eb8aa40dd67397079b1208238ae5
Christopher Najafi <chris.najafi@gmail.com>
2019-11-08 22:40:37 -0500

    fixed typo

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -8,7 +8,7 @@ const About = () => {
 				Hello! Welcome to my portfolio project. If you would like to see my
 				other projects and learn more about me, please visit{" "}
 				<a
-					href="https://wwww.chrisnajafi.com/"
+					href="https://www.chrisnajafi.com/"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
