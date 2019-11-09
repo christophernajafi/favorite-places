2711b418670929107fc403680f153d3db55a8818
Christopher Najafi <chris.najafi@gmail.com>
2019-11-09 13:03:38 -0500

    formatted about page and fixed typo

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -5,9 +5,10 @@ const About = () => {
 	return (
 		<Fragment>
 			<div className="jumbotron">
-				<p className="my-1">
-					Hello! Welcome to my portfolio project. If you would like to see my
-					other projects and learn more about me, please visit{" "}
+				<p className="my-1">Hello! Welcome to my portfolio project.</p>
+				<p>
+					If you would like to see my other projects and learn more about me,
+					please visit{" "}
 					<a
 						href="https://www.chrisnajafi.com/"
 						target="_blank"
--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -9,7 +9,7 @@ const AlertState = (props) => {
 
 	const [state, dispatch] = useReducer(alertReducer, initialState);
 
-	// Ser Alert
+	// Set Alert
 	const setAlert = (msg, type, timeout = 5000) => {
 		const id = uuid.v4();
 		dispatch({
