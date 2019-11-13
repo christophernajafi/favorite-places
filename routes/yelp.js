779377ffdf1814868a2bf803ca1b05f629cf1fd3
Christopher Najafi <chris.najafi@gmail.com>
2019-11-13 14:39:38 -0500

    cleaned up code

--- routes/yelp.js
+++ routes/yelp.js
@@ -1,41 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth");
-const { check, validationResult } = require("express-validator");
 const axios = require("axios");
 require("dotenv").config();
 
-router.get(
-	"/",
-	// auth,
+router.get("/", async (req, res, err) => {
+  try {
+    const endpoint = "https://api.yelp.com/v3/businesses/search?";
 
-	async (req, res, err) => {
-		try {
-			const endpoint = "https://api.yelp.com/v3/businesses/search?";
+    const yelpApiKey = process.env.YELP_API_KEY;
+    axios.defaults.headers.common["Authorization"] = `Bearer ${yelpApiKey}`;
 
-			const yelpApiKey = process.env.YELP_API_KEY;
-			axios.defaults.headers.common["Authorization"] = `Bearer ${yelpApiKey}`;
+    // location
+    const reqLocation = req.query.location;
+    const strLocation = reqLocation.split(" ").join("+");
+    const location = "location=" + strLocation;
 
-			// location
-			const reqLocation = req.query.location;
-			const strLocation = reqLocation.split(" ").join("+");
-			const location = "location=" + strLocation;
+    // term
+    const reqTerms = req.query.terms;
+    const strTerms = reqTerms.split(" ").join("+");
+    const term = "term=" + strTerms;
 
-			// term
-			const reqTerms = req.query.terms;
-			const strTerms = reqTerms.split(" ").join("+");
-			const term = "term=" + strTerms;
+    const fullEndpoint = endpoint + location + "&" + term;
 
-			const fullEndpoint = endpoint + location + "&" + term;
-
-			const { data } = await axios.get(fullEndpoint);
-			// console.log("response", data.businesses[0]);
-			res.status(200).json(data);
-		} catch {
-			console.error(err.message);
-			res.status(500).send("Server Error");
-		}
-	}
-);
+    const { data } = await axios.get(fullEndpoint);
+    res.status(200).json(data);
+  } catch {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
