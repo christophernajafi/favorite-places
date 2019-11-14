4bce85566026f68783971519465201abd643ef89
Christopher Najafi <chris.najafi@gmail.com>
2019-11-13 20:49:46 -0500

    converted log in and sign up components to classes

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -17,8 +17,8 @@ const NavbarComponent = props => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/about">About</Nav.Link>
-          <Nav.Link href="#">Log In</Nav.Link>
-          <Nav.Link href="#">Sign Up</Nav.Link>
+          <Nav.Link href="/log-in">Log In</Nav.Link>
+          <Nav.Link href="/sign-up">Sign Up</Nav.Link>
         </Nav>
         {props.searchResults.length ? <SearchBar /> : <div></div>}
       </Navbar.Collapse>
