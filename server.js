9cb37f3593c04e13598a062672a20019c3baaeea
Christopher Najafi <chris.najafi@gmail.com>
2019-11-13 20:15:45 -0500

    cleaned up server code, converted log in component to class

--- server.js
+++ server.js
@@ -9,34 +9,25 @@ const app = express();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// app.get("/", (req, res) => res.send(`<h1>Server started on port ${PORT}</h1>`));
-
-// CORS
-// app.use("/api/yelp", (req, res, next) => {
-// 	res.header("Access-Control-Allow-Origin", "*");
-// 	next();
-// });
-
 // Define routes
 app.use("/api/yelp", require("./routes/yelp"));
-// app.use("/api/users", require("./routes/users"));
-// app.use("/api/auth", require("./routes/auth"));
-// app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-	// Set static folder
-	app.use(express.static("client/build"));
-	app.get("*", (req, res) =>
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-	);
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-	console.log("############################################");
-	console.log(`-------- Server started on port ${PORT} -------`);
-	console.log(`---------- http://localhost:${PORT} -----------`);
-	console.log("############################################");
+  console.log("############################################");
+  console.log(`-------- Server started on port ${PORT} -------`);
+  console.log(`---------- http://localhost:${PORT} -----------`);
+  console.log("############################################");
 });
