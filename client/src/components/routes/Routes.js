7b8343dcd729fc0e3f26ffbf267d69dee9c37f78
Christopher Najafi <chris.najafi@gmail.com>
2019-11-15 11:37:32 -0500

    added settings route and component, updated gitignore files

--- client/src/components/routes/Routes.js
+++ client/src/components/routes/Routes.js
@@ -6,19 +6,21 @@ import About from "../about/About";
 import LogIn from "../log-in/LogIn";
 import SignUp from "../sign-up/SignUp";
 import Lists from "../lists/Lists";
+import Settings from "../settings/Settings";
 import NotFoundPage from "../not-found-page/NotFoundPage";
 
 const Routes = () => {
-	return (
-		<Switch>
-			<Route exact path="/" component={FrontPage} />
-			<Route exact path="/about" component={About} />
-			<Route exact path="/log-in" component={LogIn} />
-			<Route exact path="/sign-up" component={SignUp} />
-			<Route exact path="/lists" component={Lists} />
-			<Route component={NotFoundPage} />
-		</Switch>
-	);
+  return (
+    <Switch>
+      <Route exact path="/" component={FrontPage} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/log-in" component={LogIn} />
+      <Route exact path="/sign-up" component={SignUp} />
+      <Route exact path="/lists" component={Lists} />
+      <Route exact path="/settings" component={Settings} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  );
 };
 
 export default Routes;
---
+++ client/src/components/settings/Settings.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Settings = () => {
+  return (
+    <Fragment>
+      <div className="jumbotron">Settings</div>
+    </Fragment>
+  );
+};
+
+export default Settings;
