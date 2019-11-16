dcda1fc8fc72e8ae27f58b38d1013da0c14416b9
Christopher Najafi <chris.najafi@gmail.com>
2019-11-16 00:40:31 -0500

    cleaned up code in search results component

--- client/src/components/search-results/SearchResults.js
+++ client/src/components/search-results/SearchResults.js
@@ -2,26 +2,27 @@ import React from "react";
 import { connect } from "react-redux";
 import SingleSearchResult from "./single-result/SingleSearchResult";
 
-const SearchResults = (props) => {
-	return props.searchResults.length ? (
-		<div className="container mt-5">
-			<ul className="list-group mb-4">
-				{props.searchResults.map((result) => (
-					<li key={result.id} className="list-group-item">
-						<SingleSearchResult result={result} />
-					</li>
-				))}
-			</ul>
-		</div>
-	) : (
-		<div></div>
-	);
+const SearchResults = props => {
+  const { searchResults } = props;
+  return searchResults.length ? (
+    <div className="container mt-5">
+      <ul className="list-group mb-4">
+        {searchResults.map(result => (
+          <li key={result.id} className="list-group-item">
+            <SingleSearchResult result={result} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <div></div>
+  );
 };
 
-const mapStateToProps = (state) => {
-	return {
-		searchResults: state.search.searchResults
-	};
+const mapStateToProps = state => {
+  return {
+    searchResults: state.search.searchResults
+  };
 };
 
 export default connect(mapStateToProps)(SearchResults);
