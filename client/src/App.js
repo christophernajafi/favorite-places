5039cdc6bb4efadb776b71cb9cb9c06a8efd91ee
Christopher Najafi <chris.najafi@gmail.com>
2019-11-16 22:25:38 -0500

    set auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { Fragment } from "react";
-import "./App.css";
+// import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Location from "./components/location/Location";
 import Routes from "./components/routes/Routes";
 // import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Alerts from "./components/alerts/Alerts";
+import setAuthToken from "./utils/setAuthToken";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
