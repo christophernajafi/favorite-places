e0f6d4a24097fbbee3d8c96112b39687815faf33
Christopher Najafi <chris.najafi@gmail.com>
2019-12-29 19:02:24 -0500

    setup cloudinary routes

---
+++ routes/cloudinary.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+require("dotenv").config();
+
+const preset = {};
+
+const uploadProfilePic = () => {};
+
+const deleteProfilePic = () => {};
+
+router.get("/profile-pic");
+
+router.post("/profile-pic");
+
+router.delete("/profile-pic");
+
+module.exports = router;
