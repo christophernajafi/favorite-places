19cd9bb8b105f73f1de6324682da6cd3de558210
Christopher Najafi <chris.najafi@gmail.com>
2020-01-13 12:27:00 -0500

    added input boxes for passwords and button

--- client/src/components/change-password/ChangePassword.js
+++ client/src/components/change-password/ChangePassword.js
@@ -1,7 +1,56 @@
 import React, { Fragment } from "react";
+import { Button, FormGroup, FormControl } from "react-bootstrap";
 
 const ChangePassword = () => {
-  return <Fragment>Change Password Page</Fragment>;
+  return (
+    <Fragment>
+      <div className="Signup">
+        <form>
+          <FormGroup controlId="currentPassword" size="lg">
+            <FormControl
+              // value={password}
+              // onChange={onChange}
+              type="password"
+              placeholder="Current Password"
+              required
+              name="currentPassword"
+              // defaultValue={currentPassword}
+            />
+          </FormGroup>
+          <FormGroup controlId="newPassword" size="lg">
+            <FormControl
+              // value={password}
+              // onChange={onChange}
+              type="password"
+              placeholder="New Password"
+              required
+              name="newPassword"
+              // defaultValue={newPassword}
+            />
+          </FormGroup>
+          <FormGroup controlId="confirmPassword" size="lg">
+            <FormControl
+              // value={password}
+              // onChange={onChange}
+              type="password"
+              placeholder="Confirm Password"
+              required
+              name="confirmPassword"
+              // defaultValue={confirmPassword}
+            />
+          </FormGroup>
+          <Button
+            block
+            size="lg"
+            // disabled={!(currentPassword && newPassword && confirmPassword)}
+            type="submit"
+          >
+            Change Password
+          </Button>
+        </form>
+      </div>
+    </Fragment>
+  );
 };
 
 export default ChangePassword;
