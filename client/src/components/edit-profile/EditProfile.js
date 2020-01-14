a0f36a014ad3d9c7b027e35d9a086c202a889e64
Christopher Najafi <chris.najafi@gmail.com>
2020-01-13 19:53:23 -0500

    added form to edit profile

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import { Button, Form } from "react-bootstrap";
 
 import ImageUpload from "../image-upload/ImageUpload";
 
@@ -11,10 +12,29 @@ const EditProfile = () => {
             <p>
               <ImageUpload />
             </p>
-            <p>Name</p>
-            <p>Website</p>
-            <p>Bio</p>
-            <p>Email</p>
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="" />
+            </Form.Group>
+            <Form.Group controlId="website">
+              <Form.Label>Website</Form.Label>
+              <Form.Control type="text" placeholder="" />
+            </Form.Group>
+            <Form.Group controlId="bio">
+              <Form.Label>Bio</Form.Label>
+              <Form.Control type="text" placeholder="" />
+            </Form.Group>
+            <Form.Group controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Save
+            </Button>
+            <Button variant="secondary" type="submit">
+              Cancel
+            </Button>
           </div>
         </div>
       </div>
