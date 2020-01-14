2d0ae3757a3f4b6dc518a91cfc536452ae67c1ec
Christopher Najafi <chris.najafi@gmail.com>
2020-01-13 20:06:01 -0500

    added onclick to buttons

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -3,39 +3,47 @@ import { Button, Form } from "react-bootstrap";
 
 import ImageUpload from "../image-upload/ImageUpload";
 
-const EditProfile = () => {
+const EditProfile = props => {
+  const save = () => {
+    // eslint-disable-next-line
+    props.history.push("/settings");
+    // save should push to the view profile page
+  };
+
+  const cancel = () => {
+    // eslint-disable-next-line
+    props.history.push("/settings");
+  };
+
   return (
     <Fragment>
       <div className="container">
         <div className="jumbotron">
-          <div>
-            <p>
-              <ImageUpload />
-            </p>
-            <Form.Group controlId="name">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="" />
-            </Form.Group>
-            <Form.Group controlId="website">
-              <Form.Label>Website</Form.Label>
-              <Form.Control type="text" placeholder="" />
-            </Form.Group>
-            <Form.Group controlId="bio">
-              <Form.Label>Bio</Form.Label>
-              <Form.Control type="text" placeholder="" />
-            </Form.Group>
-            <Form.Group controlId="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="" />
-            </Form.Group>
+          <ImageUpload />
+          <p />
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="" />
+          </Form.Group>
+          <Form.Group controlId="website">
+            <Form.Label>Website</Form.Label>
+            <Form.Control type="text" placeholder="" />
+          </Form.Group>
+          <Form.Group controlId="bio">
+            <Form.Label>Bio</Form.Label>
+            <Form.Control type="text" placeholder="" />
+          </Form.Group>
+          <Form.Group controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="" />
+          </Form.Group>
 
-            <Button variant="primary" type="submit">
-              Save
-            </Button>
-            <Button variant="secondary" type="submit">
-              Cancel
-            </Button>
-          </div>
+          <Button variant="primary" type="submit" onClick={save}>
+            Save
+          </Button>
+          <Button variant="secondary" type="submit" onClick={cancel}>
+            Cancel
+          </Button>
         </div>
       </div>
     </Fragment>
